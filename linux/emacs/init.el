85f800430419fa9b1c63c49ed5dd841f5634a32d
DanielChabrowski <danchabrowski@gmail.com>
2020-05-26 02:59:00 +0200

    Port emacs to use-package

--- linux/emacs/init.el
+++ linux/emacs/init.el
@@ -4,14 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(package-selected-packages
-   (quote
-    (markdown-preview-mode gitlab-ci-mode-flycheck gitlab-ci-mode cmake-mode smart-mode-line racer flycheck-rust rust-mode visual-regexp undo-tree highlight-symbol neotree dockerfile-mode magit py-autopep8 elpy cff xclip buffer-move move-text zoom-window flycheck-clang-tidy crux goto-line-preview smartparens clang-format yasnippet helm helm-swoop company-lsp company ccls lsp-ui lsp-mode))))
+   '(use-package treemacs-magit treemacs-projectile lsp-treemacs treemacs markdown-preview-mode gitlab-ci-mode-flycheck gitlab-ci-mode cmake-mode smart-mode-line racer flycheck-rust rust-mode visual-regexp undo-tree highlight-symbol dockerfile-mode magit py-autopep8 elpy cff xclip buffer-move move-text zoom-window flycheck-clang-tidy crux goto-line-preview smartparens clang-format yasnippet helm helm-swoop company-lsp company ccls lsp-ui lsp-mode)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(when (file-exists-p custom-file)
+   (load custom-file))
 
 ;; Disable GC for startup
 (setq gc-cons-threshold (* 500 1024 1024)
@@ -89,9 +91,6 @@
 (global-set-key (kbd "<f2>") 'xref-find-definitions)
 (global-set-key (kbd "<f3>") 'xref-find-references)
 (global-set-key (kbd "<f5>") 'projectile-compile-project)
-(global-set-key (kbd "<f6>") 'helm-do-grep-ag)
-
-(global-set-key (kbd "C-x g") 'magit-status)
 
 (global-set-key (kbd "C-<up>") (lambda()
     (interactive)
@@ -101,50 +100,123 @@
     (interactive)
     (scroll-up 4)))
 
-(require 'helm)
-(helm-mode 1)
-(setq helm-follow-mode-persistent t)
-(setq helm-buffer-max-length nil)
-(setq helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
-(setq helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(setq helm-M-x-fuzzy-match t)
-(global-set-key (kbd "M-x") 'helm-M-x)
+(use-package magit
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("C-x g" . magit-status)
+        )
+)
 
-(require 'helm-swoop)
-(global-set-key (kbd "C-f") 'helm-swoop-without-pre-input)
+(use-package treemacs
+  :ensure t
+  :defer t
+  :bind
+  (:map global-map
+        ("C-x t 1" . treemacs-delete-other-windows)
+        ("C-x t t" . treemacs)
+        ("C-x t B" . treemacs-bookmark))
+)
 
-(require 'move-text)
-(global-set-key (kbd "C-S-<up>") 'move-text-up)
-(global-set-key (kbd "C-S-<down>") 'move-text-down)
+(use-package helm
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("M-x" . helm-M-x)
+              ("C-x b" . helm-buffers-list)
+              ("<f6>" . helm-do-grep-ag)
+        )
+  :init
+    (setq helm-M-x-fuzzy-match t)
+    (setq helm-follow-mode-persistent t)
+    (setq helm-buffer-max-length nil)
+    (setq helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
+    (setq helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
+  :config
+    (helm-mode 1)
+)
 
-(require 'buffer-move)
-(global-set-key (kbd "M-<up>") 'buf-move-up)
-(global-set-key (kbd "M-<down>") 'buf-move-down)
-(global-set-key (kbd "M-<left>") 'buf-move-left)
-(global-set-key (kbd "M-<right>") 'buf-move-right)
+(use-package helm-swoop
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("C-f" . helm-swoop-without-pre-input)
+        )
+)
 
-(require 'goto-line-preview)
-(global-set-key (kbd "C-l") 'goto-line-preview)
+(use-package move-text
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("C-S-<up>" . move-text-up)
+              ("C-S-<down>" . move-text-down)
+        )
+)
 
-(require 'visual-regexp)
+(use-package buffer-move
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("M-<up>" . buf-move-up)
+              ("M-<down>" . buf-move-down)
+              ("M-<left>" . buf-move-left)
+              ("M-<right>" . buf-move-right)
+        )
+)
 
-(require 'zoom-window)
-(global-set-key (kbd "C-x z") 'zoom-window-zoom)
+(use-package goto-line-preview
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("C-l" . goto-line-preview)
+        )
+)
 
-(require 'crux)
-(global-set-key (kbd "S-<delete>") 'crux-kill-whole-line)
-(global-set-key (kbd "C-d") 'crux-duplicate-current-line-or-region)
+(use-package visual-regexp
+  :ensure t
+  :defer t
+)
 
-(require 'cff)
-(global-set-key (kbd "<f4>") 'cff-find-other-file)
+(use-package zoom-window
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("C-x z" . zoom-window-zoom)
+        )
+)
 
-(require 'smartparens)
-(smartparens-global-mode)
+(use-package crux
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("S-<delete>" . crux-kill-whole-line)
+              ("C-d" . crux-duplicate-current-line-or-region)
+        )
+)
 
-(require 'xclip)
-(xclip-mode 1)
+(use-package cff
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("<f4>" . cff-find-other-file)
+        )
+)
+
+(use-package smartparens
+  :ensure t
+  :defer t
+  :config
+    (smartparens-global-mode)
+)
+
+(use-package xclip
+  :ensure t
+  :defer t
+  :config
+    (xclip-mode 1)
+)
 
 (require 'company-lsp)
 (push 'company-lsp company-backends)
@@ -157,34 +229,52 @@
 (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-gcc c/c++-cppcheck emacs-lisp-checkdoc))
 (global-flycheck-mode)
 
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-
-(require 'yasnippet)
-(add-hook 'after-init-hook 'yas-global-mode)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; python config
-(elpy-enable)
-
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; c++ config
-(defun my-c-mode-hook()
-  (require 'ccls)
-  (setq ccls-executable "/usr/local/bin/ccls")
-
-  (setq lsp-ui-sideline-show-hover nil)
-  (setq lsp-enable-file-watchers nil)
-  (lsp)
-  (lsp-mode)
-
-  (global-set-key (kbd "<f2>") 'lsp-find-definition)
-  (global-set-key (kbd "<f3>") 'lsp-find-references)
+(use-package company
+  :ensure t
+  :defer t
+  :config
+    (global-company-mode)
 )
-(add-hook 'c-mode-common-hook 'my-c-mode-hook)
+
+(use-package yasnippet
+  :ensure t
+  :defer t
+  :config
+    (yas-global-mode)
+)
+
+(use-package elpy
+  :ensure t
+  :defer t
+  :config
+    (elpy-enable)
+)
+
+(use-package py-autopep8
+  :ensure t
+  :defer t
+  :config
+    (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+)
+
+(use-package ccls
+  :ensure t
+  :defer t
+  :bind (:map global-map
+              ("<f2>" . lsp-find-definition)
+              ("<f3>" . lsp-find-references)
+              )
+  :init
+    (setq ccls-executable "/usr/local/bin/ccls")
+    (setq lsp-ui-sideline-show-hover nil)
+    (setq lsp-enable-file-watchers nil)
+    (add-hook 'c-mode-common-hook
+              (lambda()
+                (lsp)
+                (lsp-mode)
+                )
+              )
+)
 
 (defun clang-format-buffer-when-used()
   "Only use clang-format when it's in the project root."
@@ -195,13 +285,19 @@
 (add-hook 'c++-mode-hook
   (lambda () (add-to-list 'write-contents-functions 'clang-format-buffer-when-used)))
 
-;; dockerfile
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(use-package dockerfile-mode
+  :ensure t
+  :defer t
+  :init
+    (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+)
 
-;; yaml
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(use-package yaml-mode
+  :ensure t
+  :defer t
+  :init
+    (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+)
 
 (setq gdb-many-windows t
       gdb-use-separate-io-buffer t)
@@ -216,23 +312,31 @@
                 (jump-to-register gud-window-register)
                 (bury-buffer))))
 
-(add-hook 'rust-mode-hook #'flycheck-rust-setup)
-
-(defun rust-setup()
-    (racer-mode)
-    (global-set-key (kbd "<f2>") 'racer-find-definition)
-    (setq rust-format-on-save t)
+(use-package racer
+  :ensure t
+  :config
+    (add-hook 'racer-mode-hook #'company-mode)
 )
-(add-hook 'rust-mode-hook 'rust-setup)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(use-package rust-mode
+  :ensure t
+  :init
+    (setq rust-format-on-save t)
+  :config
+    (add-hook 'rust-mode-hook #'racer-mode)
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (add-hook 'rust-mode-hook #'flycheck-rust-setup)
+    (global-set-key (kbd "<f2>") 'racer-find-definition)
+)
+
+(use-package flycheck-rust
+  :ensure t
+  :config
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+    (add-hook 'rust-mode-hook 'flycheck-mode)
+)
 
 ;; https://stackoverflow.com/questions/1771102/changing-emacs-forward-word-behaviour
-(require 'cl)
 (defun my-syntax-class (char)
   "Return ?s, ?w or ?p depending or whether CHAR is a white-space, word or punctuation character."
   (pcase (char-syntax char)
@@ -257,7 +361,7 @@ With prefix argument ARG, do it ARG times if positive, or move backwards ARG tim
          prev-char next-char)
     (while (> count 0)
       (setq next-char (funcall char-next))
-      (loop
+      (cl-loop
        (if (or                          ; skip one char at a time for whitespace,
             (eql next-char ?\n)         ; in order to stop on newlines
             (eql (char-syntax next-char) ?\s))
@@ -290,7 +394,7 @@ With prefix argument ARG, do it ARG times if positive, or move backwards ARG tim
              (not (eql (my-syntax-class prev-char) ?s))
              (eql (my-syntax-class next-char) ?s))
             )
-         (return))
+         (cl-return))
        )
       (setq count (1- count)))))
 
